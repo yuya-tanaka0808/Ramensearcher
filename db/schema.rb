1278ae7ffaa424549ec07813e95af4cfb4021d7a
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-08 22:33:59 +0900

    [add]:cityモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_062638) do
+ActiveRecord::Schema.define(version: 2020_10_08_125750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name", null: false
