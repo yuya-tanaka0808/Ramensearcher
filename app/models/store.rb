bd1ef5e73371b5ee00f30b0651f351661e04dfe0
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-08 18:29:26 +0900

    [add]:storeのmodelのvalidationを記入

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,6 @@
 class Store < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :line_name, presence: true
+  validates :station, presence:  true
 end
