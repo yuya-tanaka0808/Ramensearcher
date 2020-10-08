1a8870238aafcd9be11417c0af161d6a495e5c91
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-08 15:18:32 +0900

    [add]:storeのroutes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'stores/index'
-  get 'stores/new'
-  get 'stores/show'
-  get 'stores/edit'
+  resources :stores
+  root to: "stores#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
