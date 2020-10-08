acc14c1e0036320c98fe5dafb137f0548e7afaa9
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-08 15:27:23 +0900

    [fix]:storeのcontroller修正

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
   before_action :find_store, only: [:show, :edit, :update, :destroy]
   def index
-    @stores = Stores.all
+    @stores = Store.all
   end
 
   def new
