4059ea2c7ead6556943907c470a7609747f5398f
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-18 00:32:59 +0900

    [add]:Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_144643) do
+ActiveRecord::Schema.define(version: 2020_10_17_152306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 2020_10_11_144643) do
     t.bigint "public_holiday_id"
     t.index ["city_id"], name: "index_stores_on_city_id"
     t.index ["public_holiday_id"], name: "index_stores_on_public_holiday_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "images", "stores"
