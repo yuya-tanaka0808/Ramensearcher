40b79ce826d8c260badb9f866770076dcaf5eb42
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-18 23:03:46 +0900

    [add]:Userテーブルにnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_004804) do
+ActiveRecord::Schema.define(version: 2020_10_18_135814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_10_18_004804) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
