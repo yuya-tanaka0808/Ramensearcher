541e3e4b67f5fe5e6d4d9f8ab32b1a8bce448024
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-19 00:17:27 +0900

    [add]:Favoliteテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-          :validatable
+  devise :database_authenticatable, :registerable, :validatable
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_stores, through: :favorites, source: :store
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_135814) do
+ActiveRecord::Schema.define(version: 2020_10_18_150507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string "name", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "images", force: :cascade do |t|
