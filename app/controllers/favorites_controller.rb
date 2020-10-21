e01fc331fb4484c862c0a8bc2ddde47bc9919d5a
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-21 10:57:14 +0900

    [fix]:お気に入り機能バグ修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(store_id: params[:store_id])
-    redirect_to stores_url, notice: "#{favorite.store.name}をお気に入り登録しました"
+    redirect_to store_path(favorite.store.id), notice: "#{favorite.store.name}をお気に入り登録しました"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to stores_url, notice: "#{favorite.store.name}をお気に入り解除しました"
+    redirect_to store_path(favorite.store.id), notice: "#{favorite.store.name}をお気に入り解除しました"
   end
 end
