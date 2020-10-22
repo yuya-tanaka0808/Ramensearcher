19cb21a46069d909b18de4d48c5112bb18694555
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-22 11:06:12 +0900

    [add]:errorページ実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(store_id: params[:store_id])
-    redirect_to store_path(favorite.store.id), notice: t('view.models.favorite.create')
+    redirect_to store_path(favorite.store.id), notice: t('view.models.favorite.created')
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
