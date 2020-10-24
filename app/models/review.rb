4e6e2897fd3557a627d7c0df8aa64431cf2f2a2f
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-10-24 16:57:05 +0900

    [add]:reviewモデルにバリデーション追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 5}
+  validates :comment, presence: true
   belongs_to :user
   belongs_to :store
   mount_uploader :image, ImageUploader
