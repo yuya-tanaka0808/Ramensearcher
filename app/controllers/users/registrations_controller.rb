184ed30b13a54e666899acf9ab5416b47837a245
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-11-18 15:29:30 +0900

    [add]:Userモデル・コントローラにomniauthの記述を追記

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,4 +62,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def build_resource(hash={})
+    hash[:uid] = User.create_unique_string
+    super
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :registerable, :validatable,:omniauthable, omniauth_providers: %i(google)
   has_many :favorites, dependent: :destroy
   has_many :favorite_stores, through: :favorites, source: :store
   has_many :reviews, dependent: :destroy
   has_many :review_stores, through: :reviews, source: :store
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+    unless user
+      user = User.new(email: auth.info.email,
+                      provider: auth.provider,
+                      uid:      auth.uid,
+                      password: Devise.friendly_token[0, 20],
+                      )
+    end
+    user.save
+    user
+  end
 end
